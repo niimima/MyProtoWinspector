0ba2efc4e73c0b89e6e33d2c183ba91241c175af
niimima <niimi@dcinc.co.jp>
2022-12-23 20:10:47 +0900

    画面に表示されているプロセス名とハンドルをコンソールに表示するように実装

---
+++ src/MyProtoWinspector/MyProtoWinspector/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("ProcessName\tProcessHandle");
+foreach (var process in Process.GetProcesses())
+{
+    // MainWindowHandleが0の場合、ウインドウを持たないためスキップ
+    // https://learn.microsoft.com/ja-jp/dotnet/api/system.diagnostics.process.mainwindowhandle?view=net-7.0#system-diagnostics-process-mainwindowhandle
+    if (process.MainWindowHandle == IntPtr.Zero) continue;
+    Console.WriteLine($"{process.ProcessName}\t{process.MainWindowHandle}");
+}
+
